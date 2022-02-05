93b7fa8e06e4a00ebdfe03ab5f86a22408476b3d
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-02-05 17:57:20 +0530

    Time: 4 ms (88.66%), Space: 15.7 MB (5.66%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int findElement(vector<int> &a,int l,int r,int x){
-        while(l<=r){
-            int m = (l+r)/2;
-            if(a[m] == x){
-                return m;
-            }else if(a[m] < x){
-                l=m+1;
-            }else{
-                r=m-1;
-            }
-        }
-        return -1;
-    }
     vector<int> twoSum(vector<int>& numbers, int target) {
         vector<int> result;
-        for(int i=0;i<numbers.size();i++){
-            int neededNumber = target - numbers[i];            
-            int numberGot = findElement(numbers,i+1,numbers.size()-1,neededNumber);            
-            if(numberGot == -1)continue;                        
-            result.push_back(i+1);
-            result.push_back(numberGot+1);
-            break;
+        int l=0;
+        int r=numbers.size()-1;
+        while(l<r){
+            int sum =  numbers[l]+numbers[r];
+            if(sum == target){
+                result.push_back(l+1);
+                result.push_back(r+1);
+                break;
+            }else if(sum < target){
+                l++;
+            }else{
+                r--;
+            }
         }
         return result;
     }
