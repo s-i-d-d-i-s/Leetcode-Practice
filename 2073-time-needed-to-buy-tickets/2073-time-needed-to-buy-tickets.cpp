7233e2bfac1eb8b5924d73d4668e6a504853aca3
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-02-12 12:28:05 +0530

    Time: 7 ms (27.65%), Space: 7.7 MB (17.96%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,40 +1,14 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        vector<pair<int,int>> v;
-        int n = tickets.size();
-        for(int i=0;i<n;i++){
-            v.push_back({tickets[i],i});
-        }
-        sort(v.begin(),v.end(),[](pair<int,int> &a,pair<int,int> &b){
-            if(a.first == b.first){
-                return a.second < b.second;
-            }
-            return a.first < b.first;
-        });
-        int buf=0;
-        for(int i=k+1;i<n;i++){
-            if(tickets[i]>=tickets[k]){
-                buf++;
-            }
-        }
-        // for(auto x:v){
-        //     cout << "(" << x.first << "," << x.second << "), ";
-        // }
-        // cout << endl;
-        int gone = 0;
         int res=0;
-        for(int i=0;i<n;i++){
-            int left = n-i;
-            int cur = v[i].first-gone;
-            if(cur>0){
-                gone += cur;
-                res += left*cur;
-            }
-            if(v[i].second == k){
-                break;
+        for(int i=0;i<tickets.size();i++){
+            if(i<=k){
+                res += min(tickets[k],tickets[i]);
+            }else{
+                res += min(tickets[k]-1,tickets[i]);
             }
         }
-        return res-buf;
+        return res;
     }
 };
