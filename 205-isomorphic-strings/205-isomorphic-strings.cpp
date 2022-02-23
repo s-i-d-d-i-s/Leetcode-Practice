083f94321b1cd987f4d92048c3997a98116fb55c
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-02-23 09:36:10 +0530

    Time: 10 ms (48.37%), Space: 6.9 MB (92.51%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> mp,mp2;
+        int mp[256],mp2[256];
+        memset(mp,-1,sizeof mp);
+        memset(mp2,-1,sizeof mp2);
         int n = s.length();
         if(t.length() != n)return false;
         for(int i=0;i<n;i++){
-            if(mp.find(s[i]) != mp.end()){
-                if(mp[s[i]] != t[i])
+            if(mp[s[i]] != -1){
+                if(mp[s[i]] != (t[i]))
                     return false;
             }
-            if(mp2.find(t[i]) != mp2.end()){
-                if(mp2[t[i]] != s[i])
+            if(mp2[t[i]] != -1){
+                if(mp2[t[i]] != (s[i]))
                     return false;
             }
             mp[s[i]] = t[i];
