9fc182b5f27674fc7fcce5197aad306c430776e7
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-02-28 19:47:59 +0530

    Time: 8 ms (42.60%), Space: 11.6 MB (76.96%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<string> result;
     bool isValid(string &a){
         int balance =0 ;
         for(char x:a){
@@ -12,24 +13,37 @@ public:
         }
         return balance == 0;
     }
-    vector<string> generateParenthesis(int n) {
-        int lim = powl(2,2*n);
-        vector<string> result;
-        for(int i=0;i<lim;i++){
-            int num = i;
-            string cur;
-            for(int j=0;j<2*n;j++){
-                if(num&1){
-                    cur.push_back('(');
-                }else{
-                    cur.push_back(')');
-                }
-                num/=2;
+    void generate(int open,int close,string &current){
+        if(open==0 and close==0){
+            if(isValid(current)){
+                result.push_back(current);
             }
-            if(isValid(cur)){
-                result.push_back(cur);
-            }
+            return;
         }
+        if(open==0){
+            current.push_back(')');
+            generate(open,close-1,current);
+            current.pop_back();
+            return;
+        }
+        if(close==0){
+            current.push_back('(');
+            generate(open-1,close,current);
+            current.pop_back();
+            return;
+        }
+        current.push_back(')');
+        generate(open,close-1,current);
+        current.pop_back();
+        
+        current.push_back('(');
+        generate(open-1,close,current);
+        current.pop_back();
+        return ;
+    }
+    vector<string> generateParenthesis(int n) {
+        string current;
+        generate(n,n,current);
         return result;
     }
 };
