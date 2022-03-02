7636d80891d6d57608e8a100ed298cbd80853771
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-02 10:37:06 +0530

    Time: 77 ms (67.20%), Space: 13.7 MB (30.43%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-        bool binarySearchFind(vector<int> &nums,int l,int r,long long key){
-        while(l<=r){
-            int m = (l+r)/2;
-            if(nums[m]>key){
-                r=m-1;
-            }else if(nums[m]<key){
-                l=m+1;
-            }else{
-                return true;
-            }
-        }
-        return false;
-    }
-    vector<vector<int>> twoSum(vector<int> &nums,int l,int r,long long target){
+    vector<vector<int>> nMinus1Sum(vector<int> &nums,int l,int r,int target,int n){
         vector<vector<int>> result;
-        for(int i=l;i<=r;i++){
-            if(i-1>=l and nums[i-1]==nums[i]){
-                continue;
+        if(n==2){
+            while(l<r){
+                int sum = nums[l]+nums[r];
+                if(sum > target){
+                    r--;
+                }else if(sum < target){
+                    l++;
+                }else{
+                    int num1 = nums[l];
+                    int num2 = nums[r];
+                    result.push_back({num1,num2});
+                    while(l<=r and nums[l] == num1){
+                        l++;
+                    }
+                    while(r>=l and nums[r] == num2){
+                        r--;
+                    }
+                }
             }
-            // x + y = target
-            long long neededNumber = target - nums[i];
-            if(binarySearchFind(nums,i+1,r,neededNumber)){
-                result.push_back({nums[i],(int)neededNumber});
-            }
-        }
-        return result;
-    }
-    
-    vector<vector<int>> threeSum(vector<int>& nums,int l,int r,long long neededTarget) {
-        vector<vector<int>> result;
-        sort(nums.begin(),nums.end());
-        for(int i=l;i<=r;i++){
-            if(i-1>=l and nums[i]==nums[i-1])continue;
-            long long neededTarget2 = neededTarget-nums[i];
-            auto solution2Sum = twoSum(nums,i+1,nums.size()-1,neededTarget2);
-            for(auto x:solution2Sum){
-                result.push_back(x);
-                result.back().insert(result.back().begin(),nums[i]);
+        }else{
+            for(int i=l;i<=r;i++){
+                if(i-1>=l and nums[i]==nums[i-1])continue;
+                long long neededTarget = target-nums[i];
+                auto solutionNMinus1Sum = nMinus1Sum(nums,i+1,nums.size()-1,neededTarget,n-1);
+                for(auto x:solutionNMinus1Sum){
+                    result.push_back(x);
+                    result.back().push_back(nums[i]);
+                }
             }
         }
         return result;
@@ -45,17 +37,6 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> result;
         sort(nums.begin(),nums.end());
-        int l=0;
-        int r=nums.size()-1;
-        for(int i=l;i<=r;i++){
-            if(i-1>=l and nums[i]==nums[i-1])continue;
-            long long neededTarget = target-nums[i];
-            auto solution2Sum = threeSum(nums,i+1,nums.size()-1,neededTarget);
-            for(auto x:solution2Sum){
-                result.push_back(x);
-                result.back().insert(result.back().begin(),nums[i]);
-            }
-        }
-        return result;         
+        return nMinus1Sum(nums,0,nums.size()-1,target,4);         
     }
 };
