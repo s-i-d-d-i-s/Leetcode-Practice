00f5dceb763c14bda4efd16f2a400ecc561b1464
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-03 22:06:53 +0530

    Time: 3 ms (66.63%), Space: 7.5 MB (16.43%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-     int getSubarrays(int n){
+    int sum(int m,int n){
+        return n*m-(n*(n+1))/2 + n;
+    }
+    int getSubarrays(int n){
         n++;
-        int res=0;
-        for(int i=3;i<=n;i++){
-            res += (n-i+1);
-        }
+        if(n<3)return 0;        
+        int res= sum(n,n) - sum(n,2);
         return res;
     }
     int numberOfArithmeticSlices(vector<int>& nums) {
         if(nums.size()<=2) return 0;
+        
         int result = 0;
         
         vector<int> diff;
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<nums.size();i++)
             diff.push_back(nums[i]-nums[i-1]);
-        }
+        
         int last=INT_MIN;
         int count=0;        
         
@@ -29,9 +31,9 @@ public:
             }
         }
         
-        if(count > 0){
+        if(count > 0)
             result += getSubarrays(count);
-        }
+        
         return result;
     }
 };
