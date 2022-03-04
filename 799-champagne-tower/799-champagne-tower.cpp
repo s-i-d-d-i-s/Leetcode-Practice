d6ee45b9642959b2545c9af6f864a073ff156107
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-04 20:03:26 +0530

    Time: 11 ms (63.80%), Space: 10.3 MB (62.28%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<double> getNextRow(vector<double> &row){
+        int n = row.size();
+        vector<double> result(n+1,0.0);
+        for(int i=0;i<n;i++){
+            double excess = 0;
+            if(row[i]>1)
+                excess = row[i]-1.0;
+            result[i]+=excess/2.0;
+            result[i+1]+=excess/2.0;
+        }
+        return result;
+    }
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> currentRow;
+        if(query_row > 0)
+            currentRow.push_back(poured);
+        else
+            currentRow.push_back(min(1.0,(double)poured));
+
+        while(query_row-->0){
+            currentRow = getNextRow(currentRow);
+        }
+        return min(1.0,currentRow[query_glass]);
+    }
+};
