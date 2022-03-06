8f3a789cf763eb2f851cfb56932cc5e3de05ec78
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-06 10:58:38 +0530

    Time: 287 ms (11.43%), Space: 111.8 MB (10.67%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        map<int,int> freq,isUp,isDown;
+        map<int,int> present,isUp,isDown;
         int n = tops.size();
         for(int i=0;i<n;i++){
-            freq[tops[i]]++;
-            isUp[tops[i]]++;
-            isDown[bottoms[i]]++;
+            // Check if it is at least present in all n domino
+            present[tops[i]]++;
             if(tops[i] != bottoms[i])
-                freq[bottoms[i]]++;
+                present[bottoms[i]]++;
+            
+            // Count the occurance on top
+            isUp[tops[i]]++;
+            
+            // Count the occurance on bottom
+            isDown[bottoms[i]]++;
         }
         int result=n+1;
-        for(auto x:freq){
-            if(x.second == n){
+        for(auto x:present){
+            if(x.second == n){ // Occurs in all domino cells
+                
+                // Flip the one on up or Flip the one on down
                 result = min(result, min(n-isDown[x.first],n-isUp[x.first]));
             }
         }
