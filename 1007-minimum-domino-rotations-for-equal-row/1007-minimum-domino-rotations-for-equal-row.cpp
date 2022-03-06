06166629227744eb782246e691358e1f7e4e9af3
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-06 11:03:50 +0530

    Time: 176 ms (61.28%), Space: 111.4 MB (94.15%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        map<int,int> present,isUp,isDown;
-        int n = tops.size();
+    
+    int minRotation(vector<int>& tops, vector<int>& bottoms,int target){
+        int make_top_equal = 0;
+        int make_bottom_equal = 0;
+        int n =  tops.size();
         for(int i=0;i<n;i++){
-            // Check if it is at least present in all n domino
-            present[tops[i]]++;
-            if(tops[i] != bottoms[i])
-                present[bottoms[i]]++;
-            
-            // Count the occurance on top
-            isUp[tops[i]]++;
-            
-            // Count the occurance on bottom
-            isDown[bottoms[i]]++;
-        }
-        int result=n+1;
-        for(auto x:present){
-            if(x.second == n){ // Occurs in all domino cells
-                
-                // Flip the one on up or Flip the one on down
-                result = min(result, min(n-isDown[x.first],n-isUp[x.first]));
+            if(tops[i] != target){
+                make_top_equal++;
             }
+            if(bottoms[i] != target){
+                make_bottom_equal++;
+            }
+            if(tops[i]!= target and bottoms[i]!=target) return n+1;
         }
+        return min(make_top_equal,make_bottom_equal);
+    }
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n =  tops.size();
+        int possible_sol_1 = tops[0];
+        int possible_sol_2 = bottoms[0];
+        int result = min(minRotation(tops,bottoms,possible_sol_1),minRotation(tops,bottoms,possible_sol_2));
         if(result == n+1) result=-1;
         return result;
     }
