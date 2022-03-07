e8d49a7a9dd0d20f93161d685fa4cc827c65da5c
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-08 02:58:29 +0530

    Time: 6 ms (59.88%), Space: 8.5 MB (93.41%) - LeetHub

--- 900-rle-iterator/900-rle-iterator.cpp
+++ 900-rle-iterator/900-rle-iterator.cpp
@@ -9,19 +9,37 @@ public:
     }
     
     int next(int n){
+        /*
+            Check if we are at a valid index
+        */
         while(index<encodedArray.size() and 
               scanned == encodedArray[index]){
             index+=2;
             scanned=0;
         }
-        if(index>=encodedArray.size()) return -1;        
-        int canHave = encodedArray[index]-scanned;
+        if(index>=encodedArray.size()) return -1;       
+        
+        // How much element we have left on the current pointer
+        int canHave = encodedArray[index]-scanned; 
+        
+        // How much element do we need to take from the current pointer
         int willHave = min(n,canHave);
+        
+        /*
+            Two cases:
+            1. we can take all 'n' elements at current pointer
+            2. we can take all elements available at current pointer,
+               but still we'll need more elements.
+        */
+        
+        // Case #1
         if(willHave == n){
             int result = encodedArray[index+1];
             scanned+=willHave;        
             return result;
         }
+        
+        // Case #2
         scanned+=willHave;
         int result = next(n-willHave);        
         return result;
