df092a7d49bfd2a5758a62f71a78a77eb501f420
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-07 14:18:40 +0530

    Time: 306 ms (37.74%), Space: 39.1 MB (10.60%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.cpp
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -1,43 +1,23 @@
 class MyCalendarTwo {
 public:
-    vector<int> start,ends;
+    map<int,int> line;
     MyCalendarTwo() {
         
     }
     
     bool book(int startTime, int endTime) {
-        vector<int> tempStart = start;
-        vector<int> tempEnd = ends;
-        
-        tempStart.push_back(startTime);
-        tempEnd.push_back(endTime);
-        
-        sort(tempStart.begin(),tempStart.end());
-        sort(tempEnd.begin(),tempEnd.end());
-
-        int result=0;
-        int current=0;
-        int i=0;
-        int j=0;
-        while(i<tempStart.size() and j<tempEnd.size()){
-            if(tempStart[i] < tempEnd[j]){
-                i++;
-                current++;
-            }else if( tempEnd[j]  < tempStart[i]){
-                j++;
-                current--;
-            }else{
-                i++;
-                j++;
+        line[startTime]++;
+        line[endTime]--;
+        int booked=0;
+        for(auto x:line){
+            booked+=x.second;
+            if(booked >= 3){
+                line[startTime]--;
+                line[endTime]++;
+                return false;
             }
-            result = max(result,current);
         }
-        if(result < 3){
-            start.push_back(startTime);
-            ends.push_back(endTime);
-            return true;
-        }
-        return false;
+        return true;
     }
 };
 
