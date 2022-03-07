21ca5725c5556955cc21a3057500044e30565516
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-07 06:09:02 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.17%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int dp[501];
+    const int MOD = 1e9+7;
     int countOrders(int n) {
-        dp[1]=1,dp[2]=6;
-		for(int i=3;i<=n;i++){
-			long long temp=0,t,u=2*i-1;
-			temp=(temp%(1000000007)+(u))%(1000000007); //put it in space inbetween
-
-			t=((2*i-2)*(2*i-1))/2%(1000000007);
-			temp=(temp%(1000000007)+t%(1000000007))%(1000000007);
-			dp[i]=(temp%(1000000007)*dp[i-1]%(1000000007))%(1000000007);
-		}
-		return dp[n];
+        long long res=1;
+        for(int i=1;i<=2*n-1;i+=2){
+            res *= i;
+            res %= MOD;
+        }
+        res *= fact(n);
+        res %= MOD;
+        return res;
+    }
+    long long fact(long long n){
+        if(n==0)return 1;
+        return (n*fact(n-1))%MOD;
     }
 };
