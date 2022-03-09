ea5bec921c5eef385413123abc91760bebb20972
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-09 06:43:59 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (66.74%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int dp[21][21][2];
+    
+    
     bool PredictTheWinner(vector<int>& nums) {
-        int total = accumulate(nums.begin(),nums.end(),0);
-        
+        memset(dp,-1,sizeof dp);
+        int total = accumulate(nums.begin(),nums.end(),0);    
         int maxScoreA = maxScoreByA(nums,0,nums.size()-1,true);
         int maxScoreB = total - maxScoreA;
         return maxScoreA >= maxScoreB;
@@ -10,14 +13,17 @@ public:
     int maxScoreByA(vector<int>& nums,int l,int r,bool isTurnForA){
         if(l>r)
             return 0;
-
+        
+        if(dp[l][r][isTurnForA] != -1){
+            return dp[l][r][isTurnForA];
+        }
         // Choose from the left
         int outcome_1 = nums[l]*isTurnForA + maxScoreByA(nums,l+1,r,isTurnForA^1);
         int outcome_2 = nums[r]*isTurnForA + maxScoreByA(nums,l,r-1,isTurnForA^1);
         
         if(isTurnForA)
-            return max(outcome_1,outcome_2);
+            return dp[l][r][isTurnForA]=max(outcome_1,outcome_2);
         
-        return min(outcome_1,outcome_2);
+        return dp[l][r][isTurnForA]=min(outcome_1,outcome_2);
     }
 };
