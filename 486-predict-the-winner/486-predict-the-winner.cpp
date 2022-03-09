746332a757dd306d74c7606c02d4129ac8077669
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-09 06:42:47 +0530

    Time: 552 ms (5.01%), Space: 7.4 MB (66.74%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        return canWin(nums,0,nums.size()-1,true);
-    }
-    bool canWin(vector<int>& nums,int l,int r,bool isTurnForA,int score=0){
-        if(l>r){
-            if(isTurnForA){
-                return score>=0;
-            }
-            return score<0;
-        }
-        // Choose from the left
-        bool outcome_1 = canWin(nums,l+1,r,isTurnForA^1,score+(isTurnForA?nums[l]:-nums[l]));
-        bool outcome_2 = canWin(nums,l,r-1,isTurnForA^1,score+(isTurnForA?nums[r]:-nums[r]));
+        int total = accumulate(nums.begin(),nums.end(),0);
         
-        return !outcome_1 or !outcome_2;
+        int maxScoreA = maxScoreByA(nums,0,nums.size()-1,true);
+        int maxScoreB = total - maxScoreA;
+        return maxScoreA >= maxScoreB;
+    }
+    int maxScoreByA(vector<int>& nums,int l,int r,bool isTurnForA){
+        if(l>r)
+            return 0;
+
+        // Choose from the left
+        int outcome_1 = nums[l]*isTurnForA + maxScoreByA(nums,l+1,r,isTurnForA^1);
+        int outcome_2 = nums[r]*isTurnForA + maxScoreByA(nums,l,r-1,isTurnForA^1);
+        
+        if(isTurnForA)
+            return max(outcome_1,outcome_2);
+        
+        return min(outcome_1,outcome_2);
     }
 };
