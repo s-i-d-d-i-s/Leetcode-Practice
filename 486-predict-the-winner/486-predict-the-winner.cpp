1a065ed194806b376cd4d08232a4ed0cc74b3773
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-09 06:35:04 +0530

    Time: 391 ms (5.01%), Space: 7.5 MB (66.74%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -3,16 +3,16 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         return canWin(nums,0,nums.size()-1,true);
     }
-    bool canWin(vector<int>& nums,int l,int r,bool isTurnForA,int scoreA=0,int scoreB=0){
+    bool canWin(vector<int>& nums,int l,int r,bool isTurnForA,int score=0){
         if(l>r){
             if(isTurnForA){
-                return scoreA>=scoreB;
+                return score>=0;
             }
-            return scoreA<scoreB;
+            return score<0;
         }
         // Choose from the left
-        bool outcome_1 = canWin(nums,l+1,r,isTurnForA^1,scoreA+(isTurnForA?nums[l]:0),scoreB + (!isTurnForA?nums[l]:0));
-        bool outcome_2 = canWin(nums,l,r-1,isTurnForA^1,scoreA+(isTurnForA?nums[r]:0),scoreB + (!isTurnForA?nums[r]:0));
+        bool outcome_1 = canWin(nums,l+1,r,isTurnForA^1,score+(isTurnForA?nums[l]:-nums[l]));
+        bool outcome_2 = canWin(nums,l,r-1,isTurnForA^1,score+(isTurnForA?nums[r]:-nums[r]));
         
         return !outcome_1 or !outcome_2;
     }
