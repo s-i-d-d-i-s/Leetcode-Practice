1bffaec4532b823188a96ed40c812c0199da287f
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-14 03:29:50 +0530

    Time: 8 ms (68.30%), Space: 26.8 MB (15.07%) - LeetHub

---
+++ 593-valid-square/593-valid-square.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        vector<vector<int>> points = {p1,p2,p3,p4};
+        sort(points.begin(),points.end());
+        do{
+            if(isValid(points[0],points[1],points[2],points[3])) return true;
+        }while(next_permutation(points.begin(),points.end()));
+        return false;
+    }
+    
+    bool isValid(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        return getDist(p1,p2)==getDist(p3,p4) and getDist(p1,p4)==getDist(p2,p3)
+               and getDist(p1,p2) == getDist(p1,p4) and getDist(p1,p3)==getDist(p2,p4)
+               and getDist(p1,p2)>0 and getDist(p2,p3)>0 and getDist(p3,p4)>0 and getDist(p1,p3)>0
+            and getDist(p2,p4)>0;
+    }
+    
+    int getDist(vector<int>& a, vector<int>& b){
+        return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
+    }
+};
