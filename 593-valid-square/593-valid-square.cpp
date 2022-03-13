c4bd93b81f3efcb7e69b92211a1753efc7e7ec30
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-14 03:31:50 +0530

    Time: 4 ms (89.95%), Space: 26.8 MB (15.07%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -3,6 +3,9 @@ public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
         vector<vector<int>> points = {p1,p2,p3,p4};
         sort(points.begin(),points.end());
+        for(int i=0;i<3;i++)
+            if(points[i]==points[i+1])return false;
+        
         do{
             if(isValid(points[0],points[1],points[2],points[3])) return true;
         }while(next_permutation(points.begin(),points.end()));
@@ -10,10 +13,9 @@ public:
     }
     
     bool isValid(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        
         return getDist(p1,p2)==getDist(p3,p4) and getDist(p1,p4)==getDist(p2,p3)
-               and getDist(p1,p2) == getDist(p1,p4) and getDist(p1,p3)==getDist(p2,p4)
-               and getDist(p1,p2)>0 and getDist(p2,p3)>0 and getDist(p3,p4)>0 and getDist(p1,p3)>0
-            and getDist(p2,p4)>0;
+               and getDist(p1,p2) == getDist(p1,p4) and getDist(p1,p3)==getDist(p2,p4);
     }
     
     int getDist(vector<int>& a, vector<int>& b){
