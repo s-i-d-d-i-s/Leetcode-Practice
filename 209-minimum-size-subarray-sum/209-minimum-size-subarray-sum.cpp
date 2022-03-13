038fec57d703114dec6028e77b28497d3e8f5bd2
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-14 04:01:45 +0530

    Time: 8 ms (78.93%), Space: 10.6 MB (14.89%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -8,17 +8,13 @@ public:
         int j=0;
         int sum = 0;
         while(i<n and j<n){            
-            while(j<n and sum < target){
-                sum += nums[j];
-                j++;                
-            }
+            sum += nums[j++];            
             while(i<n and sum>=target){
                 result = min(result,j-i);
                 sum -= nums[i];
                 i++;                
             }
-        }    
-        
+        }            
         if(result == INT_MAX) result=0;
         return result;
     }
