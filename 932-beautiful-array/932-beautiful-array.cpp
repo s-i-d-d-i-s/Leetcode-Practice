9269d07f97eb7021db6b6444136a94576f0b9636
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-14 06:24:50 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (55.17%) - LeetHub

--- 932-beautiful-array/932-beautiful-array.cpp
+++ 932-beautiful-array/932-beautiful-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> beautifulArray(int n) {
-        if(n==1){
-            return vector<int>({1});
-        }
         
-        auto old= beautifulArray(n-1);
-        vector<int> result;
-        for(int x:old){
-            int element = 2*x-1;
-            if(element<=n){
-                result.push_back(element);
+        vector<int> result={1};
+        
+        while(result.size()<n){
+            vector<int> temp;
+            for(int x: result){
+                if((2*x-1)<=n){
+                    temp.push_back(2*x-1);
+                }
             }
-        }
-        for(int x:old){
-            int element = 2*x;
-            if(element<=n){
-                result.push_back(element);
+            for(int x: result){
+                if((2*x)<=n){
+                    temp.push_back(2*x);
+                }
             }
+            result=temp;
         }
         return result;
     }
