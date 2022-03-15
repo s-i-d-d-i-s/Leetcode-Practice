2856f80311c8e928673cb8f3727ee06ddd29b58e
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-15 17:26:06 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (26.22%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -1,38 +1,36 @@
-bool isLow(char a){
-    return a>='a' and a<='z';
-}
-
 class Solution {
 public:
-    int dp[101][101];
-    bool check(int p1,int p2,string &a,string &b){
-        if(p1>=a.length()){
-            return p2>=b.length();
-        }
-        if(dp[p1][p2]!=-1){
-            return dp[p1][p2];
-        }
-        if(p2>=b.length()){
-            if(!isLow(a[p1])){
-                return dp[p1][p2]=false;
+    bool isLow(char a){
+        return a>='a' and a<='z';
+    }
+    bool isSubseq(string &query,string &pattern){
+        int pt=0,pt2=0;
+        while(pt<query.length() and pt2<pattern.length()){
+            if(query[pt]==pattern[pt2]){
+                pt++;
+                pt2++;
+            }else{
+                if(isLow(query[pt])){
+                    pt++;
+                }else{
+                    return false;
+                }
             }
-            return dp[p1][p2]=check(p1+1,p2,a,b);
         }
-        if(a[p1] == b[p2]){
-            return dp[p1][p2]=check(p1+1,p2+1,a,b);
-        }else{
-            if(!isLow(a[p1])){
-                return dp[p1][p2]=false;
+        while(pt<query.length()){
+            if(isLow(query[pt])){
+                pt++;
+            }else{
+                return false;
             }
-            return dp[p1][p2]=check(p1+1,p2,a,b);
         }
+        return pt2==pattern.length();
     }
     vector<bool> camelMatch(vector<string>& queries, string pattern) {
-        vector<bool> res;
+        vector<bool> result;
         for(auto x:queries){
-            memset(dp,-1,sizeof dp);
-            res.push_back(check(0,0,x,pattern));   
+            result.push_back(isSubseq(x,pattern));
         }
-        return res;
+        return result;
     }
 };
