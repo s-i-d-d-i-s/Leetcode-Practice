0b20b37bcb70210f87dc903781b55f1e8bc50cda
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-15 12:03:24 +0530

    Time: 1052 ms (9.91%), Space: 558.5 MB (9.42%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,16 +1,18 @@
 class TrieNode{
     public:
-        unordered_map<char,TrieNode*> child;
+        TrieNode* child[26];
         bool isTerminal;
         TrieNode(){
             isTerminal=false;
+            for(int i=0;i<26;i++){
+                child[i]=nullptr;
+            }
         }
 };
 
 class WordDictionary {
 public:
     TrieNode * root;
-    unordered_map<string,bool> cache;
     WordDictionary() {
         root= new TrieNode();
     }
@@ -18,32 +20,30 @@ public:
     void addWord(string word) {
         TrieNode * cursor = root;
         for(int i=0;i<word.length();i++){
-            if(cursor->child[word[i]] == nullptr){
-                cursor->child[word[i]] = new TrieNode();
+            if(cursor->child[word[i]-'a'] == nullptr){
+                cursor->child[word[i]-'a'] = new TrieNode();
             }
-            cursor = cursor->child[word[i]];
+            cursor = cursor->child[word[i]-'a'];
         }
         cursor->isTerminal = true;
-        cache.clear();
     }
     
     bool search(string word) {
-        if(cache.find(word) != cache.end()) return cache[word];
-        return  cache[word]=depthFirstSearch(0,word,root);
+        return depthFirstSearch(0,word,root);
     }
     
     bool depthFirstSearch(int idx,string &a,TrieNode *cursor){
         if(idx>=a.length())return cursor->isTerminal;
         if(a[idx] == '.'){
-            for(char c:"abcdefghijklmnopqrstuvwxyz"){
-                if(cursor->child[c] != nullptr){
-                    if(depthFirstSearch(idx+1,a,cursor->child[c]))return true;
+            for(char c='a';c<='z';c++){
+                if(cursor->child[c-'a'] != nullptr){
+                    if(depthFirstSearch(idx+1,a,cursor->child[c-'a']))return true;
                 }
             }
             return false;
         }
-        if(cursor->child[a[idx]] == nullptr) return false;
-        return depthFirstSearch(idx+1,a,cursor->child[a[idx]]);
+        if(cursor->child[a[idx]-'a'] == nullptr) return false;
+        return depthFirstSearch(idx+1,a,cursor->child[a[idx]-'a']);
     }
 };
 
