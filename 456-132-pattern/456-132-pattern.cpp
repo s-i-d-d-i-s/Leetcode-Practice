f294c69b9bd9a27bb7092a5f482bcd9a6d4dff2c
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-16 10:06:06 +0530

    Time: 139 ms (24.94%), Space: 39.7 MB (17.76%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return false;
+        
+        stack<pair<int,int>> stk;
+        vector<int> nextGreater(n,-1),minimum(n);
+        
+        minimum[0] = nums[0];
+        for(int i=1;i<n;i++){
+            minimum[i] = min(minimum[i-1],nums[i]);
+        }
+        
+        stk.push({nums[0],0});
+        for(int i=1;i<n;i++){
+            while(stk.size()>0 and stk.top().first<nums[i]){
+                stk.pop();
+            }
+            if(stk.size()>0){
+                nextGreater[i]=stk.top().second;
+            }
+            stk.push({nums[i],i});
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nextGreater[i] != -1){
+                int j = nextGreater[i];
+                if(j-1>=0){
+                    if(minimum[j-1]<nums[j] and nums[j]>nums[i] and minimum[j-1]<nums[i]){
+                        cout << i << " " << j << endl;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
