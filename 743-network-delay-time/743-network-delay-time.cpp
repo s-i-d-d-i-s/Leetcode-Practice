019bbcb23f6d714fa41a525208f683f0c0e125d5
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-17 02:36:26 +0530

    Time: 188 ms (45.92%), Space: 44 MB (19.05%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -2,38 +2,33 @@ class Solution {
 public:
     map<int,vector<pair<int,int>>> adjList;
     
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {        
-        vector<bool> visited(n+1,false);
-        
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         for(auto x:times)
             adjList[x[0]].push_back({x[1],x[2]});
         
         vector<int> distance(n+1,INT_MAX);
-        
-        multiset<pair<int,int>> Q;
-        
-        Q.insert({0,k});
-        visited[k]=1;
         distance[k]=0;
         
+        set<pair<int,int>> Q;
+        Q.insert({0,k});
+        
         while(Q.size()>0){
-            auto node = *Q.begin();            
+            auto [cost,node] = *Q.begin();
             Q.erase(Q.begin());
             
-            for(auto x:adjList[node.second]){                
-                if(distance[x.first] > distance[node.second]+x.second){
-                    distance[x.first] = distance[node.second]+x.second;
+            for(auto x:adjList[node]){
+                if(distance[x.first] > x.second+cost){
+                    distance[x.first] = x.second+cost;
                     Q.insert({distance[x.first],x.first});
-                    visited[x.first]=1;                    
                 }
             }
         }
-        int result=0;
-        for(int i=1;i<=n;i++){
-            if(distance[i]==INT_MAX) return -1;
+        int result=INT_MIN;
+        for(int i=1;i<=n;i++) {
+            if(distance[i] == INT_MAX) return -1;
             result = max(result,distance[i]);
         }
+        
         return result;
     }
-    
 };
