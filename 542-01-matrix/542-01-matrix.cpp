ebc3628fa237ffb5042d2cb44218da270e74a78d
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-16 08:01:00 +0530

    Time: 104 ms (54.56%), Space: 27.3 MB (95.61%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -11,31 +11,32 @@ public:
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
                     result[i][j] = 0;
-                    Q.push({i,j});
                 }
             }
         }
         
-        while(Q.size()>0){
-            auto cell = Q.front();
-            Q.pop();
-            if(cell.first-1>=0 and result[cell.first-1][cell.second] == INT_MAX){
-                result[cell.first-1][cell.second]=1+result[cell.first][cell.second];
-                Q.push({cell.first-1,cell.second});
-            }
-            if(cell.first+1<n and result[cell.first+1][cell.second] == INT_MAX){
-                result[cell.first+1][cell.second]=1+result[cell.first][cell.second];
-                Q.push({cell.first+1,cell.second});
-            }
-            if(cell.second-1>=0 and result[cell.first][cell.second-1] == INT_MAX){
-                result[cell.first][cell.second-1]=1+result[cell.first][cell.second];
-                Q.push({cell.first,cell.second-1});
-            }
-            if(cell.second+1<m and result[cell.first][cell.second+1] == INT_MAX){
-                result[cell.first][cell.second+1]=1+result[cell.first][cell.second];
-                Q.push({cell.first,cell.second+1});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 1){
+                    int up = i-1>=0?result[i-1][j]:INT_MAX;
+                    int left = j-1>=0?result[i][j-1]:INT_MAX;
+                    if(min(up,left)==INT_MAX)continue;
+                    result[i][j] = min(up,left)+1;
+                }
             }
         }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(mat[i][j] == 1){
+                    int down = i+1<n?result[i+1][j]:INT_MAX;
+                    int right = j+1<m?result[i][j+1]:INT_MAX;
+                    if(min(down,right)==INT_MAX)continue;
+                    result[i][j] = min(result[i][j], min(down,right)+1);
+                }
+            }
+        }
+        
+       
         return result;
     }
 };
