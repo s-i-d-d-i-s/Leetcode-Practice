7537476db37731a9a16fa4545442bd5a6e14a094
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-16 10:41:07 +0530

    Time: 28 ms (5.05%), Space: 15.3 MB (81.35%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        map<int,int> isPushed;
-        int i=0;
-        int j=0;
-        stack<int> stack;
-        while(i<pushed.size() and j<popped.size()){
-            if(stack.size()>0 and stack.top() == popped[j] and isPushed[popped[j]] == true){
-                stack.pop();
-                isPushed[popped[j]]=false;
-                j++;
-            }else if(isPushed[pushed[i]] == false){
-                stack.push(pushed[i]);
-                isPushed[pushed[i]]=true;
-                i++;
+        stack<int> stk;
+        int pt=0;
+        int pt2=0;
+        while(pt<pushed.size() and pt2<popped.size()){
+            if(stk.size()>0 and popped[pt2] == stk.top()){
+                pt2++;
+                stk.pop();
             }else{
-                return false;
+                stk.push(pushed[pt]);
+                pt++;
             }
         }
-        while(i<pushed.size()){
-            if(isPushed[pushed[i]] == false){
-                isPushed[pushed[i]]=true;
-                i++;
-            }else{
-                return false;
-            }
+        while(stk.size()>0 and popped[pt2] == stk.top()){
+            pt2++;
+            stk.pop();
         }
-        while(j<popped.size()){
-            if(stack.size()>0 and stack.top() == popped[j] and isPushed[popped[j]] == true){
-                stack.pop();
-                isPushed[popped[j]]=false;
-                j++;
-            }else{
-                return false;
-            }
-        }
-        return true;
+        
+        return pt>=pushed.size() and pt2>=pushed.size();
     }
 };
