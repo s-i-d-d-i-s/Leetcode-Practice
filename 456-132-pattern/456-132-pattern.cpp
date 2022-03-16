4d315b7cc71916026d6acc349bb1430f6db1b00f
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-16 10:08:05 +0530

    Time: 109 ms (43.08%), Space: 39.4 MB (19.99%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -8,26 +8,22 @@ public:
         vector<int> nextGreater(n,-1),minimum(n);
         
         minimum[0] = nums[0];
+        stk.push({nums[0],0});
+        
         for(int i=1;i<n;i++){
             minimum[i] = min(minimum[i-1],nums[i]);
-        }
-        
-        stk.push({nums[0],0});
-        for(int i=1;i<n;i++){
-            while(stk.size()>0 and stk.top().first<nums[i]){
+            
+            while(stk.size()>0 and stk.top().first<nums[i])
                 stk.pop();
-            }
-            if(stk.size()>0){
+            
+            if(stk.size()>0)
                 nextGreater[i]=stk.top().second;
-            }
-            stk.push({nums[i],i});
-        }
-        for(int i=n-1;i>=0;i--){
+            
+            stk.push({nums[i],i});                        
             if(nextGreater[i] != -1){
                 int j = nextGreater[i];
                 if(j-1>=0){
                     if(minimum[j-1]<nums[j] and nums[j]>nums[i] and minimum[j-1]<nums[i]){
-                        cout << i << " " << j << endl;
                         return true;
                     }
                 }
