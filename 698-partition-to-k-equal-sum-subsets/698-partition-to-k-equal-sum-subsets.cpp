8b2bc70aa4a39b09bea0f2d4482b352526e6e15d
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-17 10:49:35 +0530

    Time: 1232 ms (10.52%), Space: 9.4 MB (80.21%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -5,26 +5,32 @@ public:
         if(sum % k !=0 )return false;
         sum/=k;
         int mask=0;
-        //sort(nums.begin(),nums.end(),greater<int>());
-        return generate(0,nums,mask,k,0,sum);
+        return canPartition(0,nums,mask,k,0,sum);
     }
     
-    bool generate(int idx,vector<int> &nums,int mask,int k,int cur,int sumNeeded){
+    bool canPartition(int idx,vector<int> &nums,int mask,int k,int cur,int sumNeeded){
         
-        if(k==1)
-            return true;
         
-        if(idx>=nums.size())
-            return false;
+        
+        if(k==1) return true;
+        
+        if(idx>=nums.size()) return false;
+        
+        if(cur > sumNeeded) return false;
         
         if(cur == sumNeeded)
-            return generate(0,nums,mask,k-1,0,sumNeeded);
-
+            return canPartition(0,nums,mask,k-1,0,sumNeeded);
         
-        if( !(mask & (1<<idx)) and cur+nums[idx] <= sumNeeded){
-            if(generate(idx+1,nums,mask|(1<<idx),k,cur+nums[idx],sumNeeded))
-                return true;
-        }
-        return generate(idx+1,nums,mask,k,cur,sumNeeded);
+        
+
+        bool isElementVisited = mask & (1<<idx);
+        
+        if(isElementVisited)
+            return canPartition(idx+1,nums,mask,k,cur,sumNeeded);
+        
+        bool takeThisElement = canPartition(idx+1,nums,mask|(1<<idx),k,cur+nums[idx],sumNeeded);
+        bool doNotTakeThisElement = canPartition(idx+1,nums,mask,k,cur,sumNeeded);
+        
+        return takeThisElement || doNotTakeThisElement;
     }
 };
