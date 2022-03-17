70f0e60256f3b7602811cb698b2d557194807fad
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-17 13:39:19 +0530

    Time: 11 ms (87.17%), Space: 8.4 MB (66.23%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int freq[100];
+        int bufferSize='z'-'0'+1;
+        int freq[bufferSize];
+        cout << bufferSize << endl;
         memset(freq,0,sizeof freq);
         for(char x:s){            
             freq[x-'0']++;
         }
         string result="";
-        for(int i=0;i<100;i++){
+        for(int i=0;i<bufferSize;i++){
             int maxi = 0;
             char last='*';
-            for(int j=0;j<100;j++){
+            for(int j=0;j<bufferSize;j++){
                 if(freq[j]>maxi){
                     maxi=freq[j];
                     last=j+'0';
