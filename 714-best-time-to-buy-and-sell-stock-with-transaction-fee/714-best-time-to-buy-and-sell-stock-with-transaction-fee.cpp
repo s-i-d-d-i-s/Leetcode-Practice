b295dc4b74712c28f92e1706ca3983f16a805dc0
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-18 23:13:57 +0530

    Time: 161 ms (45.33%), Space: 55.5 MB (45.05%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,10 +3,10 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
         
-        int min_profit_when_already_bought[n];
+        int max_profit_when_already_bought[n];
         int max_profit_when_already_sold[n];
         
-        min_profit_when_already_bought[0] = -prices[0];
+        max_profit_when_already_bought[0] = -prices[0];
         max_profit_when_already_sold[0] = 0;
         for(int i=1;i<n;i++){
             
@@ -14,8 +14,8 @@ public:
                 min profit if we buy today           -> 1. Don't buy i.e use last day's buy min
                                                      -> 2. Buy today i.e use last day's sell max
             */
-            min_profit_when_already_bought[i] = max(
-                                                    min_profit_when_already_bought[i-1],
+            max_profit_when_already_bought[i] = max(
+                                                    max_profit_when_already_bought[i-1],
                                                     max_profit_when_already_sold[i-1]-prices[i]
                                                 );
             
@@ -25,7 +25,7 @@ public:
             */
             max_profit_when_already_sold[i] = max(
                                                     max_profit_when_already_sold[i-1],
-                                                    min_profit_when_already_bought[i-1]+prices[i]-fee
+                                                    max_profit_when_already_bought[i-1]+prices[i]-fee
                                                 );
         }
         
