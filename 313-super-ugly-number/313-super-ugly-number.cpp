48aa47a3214c158cf425b7d27a72ccf44c3c6fd8
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-18 20:31:20 +0530

    Time: 1453 ms (47.28%), Space: 157.1 MB (5.10%) - LeetHub

---
+++ 313-super-ugly-number/313-super-ugly-number.cpp
@@ -0,0 +1,39 @@
+#define ll long long
+struct cmp{
+    bool operator()(const pair<ll,pair<ll,ll>> &a,const pair<ll,pair<ll,ll>> &b) const{
+        return a.first*a.second.second < b.first*b.second.second;
+    }
+};
+
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<ll> result={1};
+        set<pair<ll,pair<ll,ll>>,cmp> pq;
+        for(auto x:primes){
+            pq.insert({x,{0,result[0]}});
+        }
+        while(result.size()<n){
+            ll num = pq.begin()->first;
+            ll pointer = pq.begin()->second.first;
+            ll pointerValue = pq.begin()->second.second;
+            
+            ll value = num*pointerValue;
+            
+            
+            pq.erase(pq.begin());
+            result.push_back(value);
+            
+            while(true){
+                pointer++;
+                pair<ll,pair<ll,ll>> newEntry = {num,{pointer,result[pointer]}};
+                if(pq.find(newEntry) == pq.end()){
+                    pq.insert(newEntry);
+                    break;
+                }
+            }
+        }
+        
+        return result[n-1];
+    }
+};
