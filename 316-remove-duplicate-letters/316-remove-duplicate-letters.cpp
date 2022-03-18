bdf8dd5b30931f54c6d554ab69c496a9af029f96
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-18 07:23:21 +0530

    Time: 8 ms (24.25%), Space: 6.5 MB (92.50%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -3,6 +3,7 @@ public:
     string removeDuplicateLetters(string s) {
         string result = "";
         int freq[26]={0};
+        int inResult[26]={0};
         
         for(char c:s)
             freq[c-'a']++;
@@ -11,13 +12,17 @@ public:
             freq[c-'a']--;
             if(result.size()==0){
                 result.push_back(c);
+                inResult[c-'a']=1;
             }else{
-                if(result.find(c) != string::npos) continue;
+                if(inResult[c-'a'] == 1) continue;
                 
                 while(result.size()>0 and freq[result.back() - 'a']>0 and result.back() > c){
+                    inResult[result.back()-'a']=0;
                     result.pop_back();
                 }
+                
                 result.push_back(c);
+                inResult[c-'a']=1;
             }
         }
         return result;
