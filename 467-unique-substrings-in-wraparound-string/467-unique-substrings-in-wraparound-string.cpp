b102fb21db61ee910a8ef9d9fe02a74099215856
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-20 07:21:02 +0530

    Time: 35 ms (7.38%), Space: 7.4 MB (21.04%) - LeetHub

---
+++ 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int findSubstringInWraproundString(string p) {
+        int n = p.length();
+        int result=0;
+        char last=p[0];
+        int len = 1;
+        map<char,int> maxSubstr;
+        
+        maxSubstr[p[0]]=max(maxSubstr[p[0]],len);
+        
+        for(int i=1;i<p.length();i++){
+            if(last == 'z'){
+                if(p[i] == 'a'){
+                    len++;
+                    last = p[i];
+                }else{
+                    len=1;
+                    last = p[i];
+                }
+            }else{
+                if(p[i] == last + 1){
+                    len++;
+                    last = p[i];
+                }else{
+                    len=1;
+                    last = p[i];
+                }
+            }
+            maxSubstr[p[i]]=max(maxSubstr[p[i]],len);
+        }
+        
+        if(len>0)
+            maxSubstr[p[n-1]]=max(maxSubstr[p[n-1]],len);
+        
+        for(auto x:maxSubstr){
+            result += x.second;
+        }
+        return result;
+    }
+    int getSubstring(int n){
+        return (n*(n+1))/2;
+    }
+};
