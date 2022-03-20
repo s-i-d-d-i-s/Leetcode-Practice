bb9c14b755176bbfa0857d4204c82fdf0e89f43c
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-20 14:10:28 +0530

    Time: 20 ms (67.08%), Space: 12.5 MB (37.15%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -1,54 +1,25 @@
 class Solution {
 public:
     int findBestValue(vector<int>& arr, int target) {
-        int n = arr.size();
-        int least = getLeast(arr,target);
-        int most = getMost(arr,target);
-       // cout << least << " " << most << endl;
-        int d1 = abs(target-getSum(arr,least));
-        int d2 = abs(target-getSum(arr,most));
-        if(d1<=d2){
-            return least;
-        }
-        return most;
-    }
-    
-    int getLeast(vector<int>& arr, int target){
-        int l=0;
-        int r=*max_element(arr.begin(),arr.end());
-        int result = 0;
-        while(l<=r){
-            int m = (l+r)/2;
-            if(getSum(arr,m)<=target){
-                result = max(result,m);
-                l=m+1;
-            }else{
-                r=m-1;
+        sort(arr.begin(),arr.end());
+        int maximum = *max_element(arr.begin(),arr.end());
+        int totalSum = accumulate(arr.begin(),arr.end(),0);
+        int result = maximum;
+        int diff = INT_MAX;
+        int right=0;
+        for(int i=maximum;i>=0;i--){
+            while(arr.size()>0 and arr.back()>=i){
+                right++;
+                totalSum -= arr.back();
+                arr.pop_back();
             }
-        }
-        return result;
-    }
-    
-    int getMost(vector<int>& arr, int target){
-        int l=0;
-        int r=*max_element(arr.begin(),arr.end());
-        int result = INT_MAX;
-        while(l<=r){
-            int m = (l+r)/2;
-            if(getSum(arr,m)<target){
-                l=m+1;
-            }else{
-                result = min(result,m);
-                r=m-1;
-            }
-        }
-        return result;
-    }
-    
-    int getSum(vector<int>& arr, int val){
-        int result=0;
-        for(int x:arr){
-            result += min(x,val);
+            int current = right*i + totalSum;
+            int current_difference = abs(current-target);
+            if(current_difference<diff){
+                diff=current_difference;
+                result = i;
+            }else if(diff == current_difference)
+                result = i;
         }
         return result;
     }
