57efb8e0f1b792f09841cb7ac8994bed84f4d808
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-21 16:46:12 +0530

    Time: 56 ms (79.41%), Space: 29.8 MB (30.15%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.cpp
+++ 962-maximum-width-ramp/962-maximum-width-ramp.cpp
@@ -4,29 +4,18 @@ public:
         vector<pair<int,int>> stk;
         stk.push_back({nums[0],0});
         int result=0;
-        for(int i=1;i<nums.size();i++){
-            int current_width = getWidth(stk,nums[i],i);
+        for(int i=1;i<nums.size();i++){            
             if(nums[i]<stk.back().first){
                 stk.push_back({nums[i],i});
             }
-            result = max(result,current_width);
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            while(stk.size()>0 and stk.back().first<=nums[i]){
+                result = max(result,i-stk.back().second);
+                stk.pop_back();
+            }
         }
         return result;
     }
-    int getWidth(vector<pair<int,int>> &stk,int target,int i){
-        if(stk.back().first>target) return INT_MIN;
-        int l=0;
-        int r=stk.size();
-        int result = stk.back().second;
-        while(l<=r){
-            int m = (l+r)/2;
-            if(stk[m].first<=target){
-                result = min(result,stk[m].second);
-                r=m-1;
-            }else{
-                l=m+1;
-            }
-        }
-        return i-result;
-    }
+    
 };
