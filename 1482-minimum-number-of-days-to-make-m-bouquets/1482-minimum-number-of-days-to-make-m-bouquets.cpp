180a88a01256338ff1dd0658c3e9c45176fb2220
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-22 10:10:35 +0530

    Time: 292 ms (28.55%), Space: 63.3 MB (27.76%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int limit = 1e9+1;
+        int l=0;
+        int r=limit;
+        int result=r;
+        while(l<=r){
+            int mid = (l+r)/2;
+            bool f = canForm(bloomDay,m,k,mid);
+            if(f){
+                result = min(result,mid);
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        if(result==limit)result=-1;
+        return result;
+    }
+    bool canForm(vector<int> &bloom,int m,int k,int date){
+        int current=0;
+        for(int i=0;i<bloom.size();i++){
+            if(date>=bloom[i]){
+                current++;
+            }else{
+                current=0;
+            }
+            if(current>=k){
+                m--;
+                current=0;
+            }
+        }        
+        return m<=0;
+    }
+};
