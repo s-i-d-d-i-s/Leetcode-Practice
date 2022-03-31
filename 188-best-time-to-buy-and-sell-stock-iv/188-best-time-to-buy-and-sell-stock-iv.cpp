4c263d269d7d5d16b1a1b9e939d42d59a73602d7
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-03-31 23:29:23 +0530

    Time: 15 ms (52.25%), Space: 11.7 MB (57.19%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int dp[1001][101];
+    int dp[1001][101][2];
     Solution(){
         memset(dp,-1,sizeof dp);
     }
-    int maxProfit(int k, vector<int>& prices,int idx=0) {
-        if(idx>=prices.size()){
+    int maxProfit(int k, vector<int>& prices,int idx=0,bool buy=true) {
+        if(idx>=prices.size() or (buy and k==0))
             return 0;
-        }
-        if(dp[idx][k] != -1){
-            return dp[idx][k];
-        }
-        int mini = prices[idx];
         
-        int result=maxProfit(k,prices,idx+1);
+        if(dp[idx][k][buy] != -1)
+            return dp[idx][k][buy];
         
-        for(int i=idx+1;i<prices.size() and k>0;i++){
-            result = max(result,prices[i]-mini + maxProfit(k-1,prices,i+1));
-        }
-        return dp[idx][k]=result;
+        int result1 = maxProfit(k,prices,idx+1,buy);
+        int result2 = prices[idx]*(buy?-1:1) + maxProfit(k-(buy?1:0),prices,idx+1,!buy);
+        
+        return dp[idx][k][buy] = max(result1,result2);
     }
 };
