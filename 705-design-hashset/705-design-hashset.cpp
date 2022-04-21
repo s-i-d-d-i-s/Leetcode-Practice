67990b952041a49cdaa9f45d179da5ef700a9989
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-04-21 07:14:43 +0530

    Time: 122 ms (65.23%), Space: 41.8 MB (54.74%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,46 @@
+#define N 1000
+class MyHashSet {
+public:
+    list<int> hashTable[N];
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        if(contains(key))return;
+        hashTable[getHash(key)].push_back(key);
+    }
+    
+    void remove(int key) {
+        auto it = hashTable[getHash(key)].begin();
+        while(it != hashTable[getHash(key)].end()){
+            if((*it)==key){
+                break;
+            }
+            it++;
+        }
+        if(it != hashTable[getHash(key)].end()){
+            hashTable[getHash(key)].erase(it);
+        }
+    }
+    
+    bool contains(int key) {
+        for(int x:hashTable[getHash(key)]){
+            if(x == key){
+                return true;
+            }
+        }
+        return false;
+    }
+    int getHash(int num){
+        return num%N;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
