ec7c818d8447f95299d3b4edc7ab7cb8019d3041
Siddharth Singh <68241942+s-i-d-d-i-s@users.noreply.github.com>
2022-05-23 09:48:28 +0530

    Time: 489 ms (35.21%), Space: 35.8 MB (47.89%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -5,9 +5,8 @@ public:
         if(idx>=strs.size())
             return 0;
         
-        if(dp[idx][c0][c1]!=-1){
+        if(dp[idx][c0][c1]!=-1)
             return dp[idx][c0][c1];
-        }
         
         int res=solve(idx+1,strs,c0,c1,m,n);
         int cur_c1=count(strs[idx].begin(),strs[idx].end(),'1');
@@ -16,7 +15,6 @@ public:
         if(c0+cur_c0<=m and c1+cur_c1<=n){
             res=max(res,1+solve(idx+1,strs,c0+cur_c0,c1+cur_c1,m,n));
         }
-        
         return dp[idx][c0][c1]=res;
     }
     int findMaxForm(vector<string>& strs, int n, int m) {
